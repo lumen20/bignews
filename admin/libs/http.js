c928df95529ab9f5d2bee61f1da001cbd0fbf522
lumen20 <1609917019@qq.com>
2020-04-10 16:42:36 +0800

    写完了登录页面与index首页

---
+++ admin/libs/http.js
@@ -0,0 +1,33 @@
+//   * 将项目所有的接口地址写在一个文件中（http.js）
+//   * 这样以后接口发生修改就不用修改每一个页面，直接修改这个文件即可
+//   * b.将http.js文件中的基地址与接口地址分开写
+//   * 因为服务器的基地址有可能会变。一般企业会有一个测试环境与正式发布环境的基地址
+
+/* 沙箱模式 */
+(function(w){
+    var baseURL = 'http://localhost:8080/api/v1'
+    var BigNew = {
+        baseURL:baseURL,//基地址
+        user_login:      baseURL + '/admin/user/login',//用户登录
+        user_info:       baseURL + '/admin/user/info',//用户信息
+        user_detail:     baseURL + '/admin/user/detail',//用户详情
+        user_edit:       baseURL + '/admin/user/edit',//用户编辑
+        category_list:   baseURL + '/admin/category/list',//文章类别查询
+        category_add:    baseURL + '/admin/category/add',//文章类别新增
+        category_search: baseURL + '/admin/category/search',//文章类别搜索
+        category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
+        category_delete: baseURL + '/admin/category/delete',//文章类别删除
+        article_query:   baseURL + '/admin/article/query',//文章搜索
+        article_publish: baseURL + '/admin/article/publish',//文章发布
+        article_search:  baseURL + '/admin/article/search',//文章信息查询
+        article_edit:    baseURL + '/admin/article/edit',//文章编辑
+        article_delete:  baseURL + '/admin/article/delete',//文章删除
+        comment_list:    baseURL + '/admin/comment/search',//文章评论列表
+        comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
+        comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
+        comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
+    };
+
+    //暴露接口
+    w.BigNew = BigNew;
+})(window);
